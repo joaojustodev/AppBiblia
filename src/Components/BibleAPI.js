838b9afb05c5fcd2f3c4a45aca1a5f23a0a8391b
Joaobr200 <joaoarantesjob@gmail.com>
2020-10-19 15:57:52 -0300

    fix

--- src/Components/BibleAPI.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-const bible = axios.create({
-	baseURL: "https://bibleapi.co/api/verses"
-});
-
-export default bible;
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: ['@babel/polyfill','./src/js/main2.js'],
+  entry: ["@babel/polyfill", "./src/js/main.js"],
   output: {
-    path: path.resolve(__dirname, './public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "./src/js"),
+    filename: "bundle.js",
   },
   devServer: {
-    contentBase: path.join(__dirname, './src/dist'),
+    contentBase: path.join(__dirname, "./src/dist"),
     compress: true,
-    port: 9000
+    port: 9000,
   },
   module: {
-  rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-proposal-object-rest-spread']
-        }
-      }
-    }
-  ]
-},
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
+    ],
+  },
 };
